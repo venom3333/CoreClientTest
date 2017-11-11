26741971e3d43f68f96d10b3da4800f58fa3a5e5
vovan <venom3333@gmail.com>
2017-11-11 19:56:07 +0300

    Базовое соединение и отправка строк.

--- CoreClientTest/Program.cs
+++ CoreClientTest/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using System.Net;
 
 namespace CoreClientTest
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var address = IPAddress.Parse("127.0.0.1");
+            var port = 5678;
+            if (args.Length > 1)
+            {
+                address = IPAddress.TryParse(args[0], out var parsedIp) ? parsedIp : address;
+                port = int.TryParse(args[1], out var parsedPort) ? parsedPort : port;
+            }
+            
+            TcpClientBase.StartClient(address, port);
         }
     }
 }
---
+++ CoreClientTest/TcpClientBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CoreClientTest
+{
+    public static class TcpClientBase
+    {
+        //private static TcpClient Client { get; set; }
+
+        public static void StartClient(IPAddress address, int port)
+        {
+            using (var client = new TcpClient())
+            {
+                client.Connect(address, port);
+
+                if (!client.Connected)
+                {
+                    Console.WriteLine($"Can't connect to {address}:{port}");
+                    return;
+                }
+                
+                Console.WriteLine($"Connected to {address}:{port}");
+
+                while (true)
+                {
+                    Console.Write($"Enter data: ");
+                    var message = Console.ReadLine();
+
+                    if (message.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+                    
+                    var sendData = Encoding.ASCII.GetBytes(message);
+
+                    client.GetStream().Write(sendData, 0, sendData.Length);
+                }
+                Console.WriteLine("Disconnecting.");
+                client.Close();
+            }
+        }
+    }
+}
